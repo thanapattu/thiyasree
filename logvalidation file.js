9872c056e84e917df1bdeb050496a0b5758f3dee
thanapattu <45780838+thanapattu@users.noreply.github.com>
2018-12-14 12:39:19 +0530

    Create logvalidation file.js

---
+++ logvalidation file.js
@@ -0,0 +1,15 @@
+function validateForm() {
+  
+  x=document.getElementById("email").value;
+  if(x==""){
+  alert("email must be filled");
+  return false;
+  }  
+  x=document.getElementById("password").value;
+  if(x==""){
+  alert("password must be filled");
+  return false;
+  }
+ 
+}
+  
